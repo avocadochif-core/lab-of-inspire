47f9adab90300a3b3b2a0066524b2ff6079af610
avocadochif-core <avocadochif@gmail.com>
2021-02-23 11:27:14 +0200

    Added click listener for item_photo

--- app/src/main/java/com/lab/of/inspire/view/recyclerview/viewholder/PhotoViewHolder.kt
+++ app/src/main/java/com/lab/of/inspire/view/recyclerview/viewholder/PhotoViewHolder.kt
@@ -8,11 +8,13 @@ import com.lab.of.inspire.databinding.ItemPhotoBinding
 import com.lab.of.inspire.entity.photo.Photo
 import com.lab.of.inspire.extensions.loadWithCache
 import com.lab.of.inspire.extensions.loadWithCacheCircled
+import com.lab.of.inspire.extensions.singleClickListener
 import com.lab.of.inspire.view.recyclerview.adapter.PhotosAdapter
 
 class PhotoViewHolder(private val binding: ItemPhotoBinding, private val listener: PhotosAdapter.PhotosListener): RecyclerView.ViewHolder(binding.root) {
 
     fun bind(photo: Photo) {
+        binding.root.singleClickListener { listener.onPhotoClick(photo) }
         binding.previewIV.setAspectRatio(photo.width, photo.height)
         photo.color?.let { binding.previewIV.background = ColorDrawable(Color.parseColor(it)) }
         Glide.with(binding.previewIV.context).loadWithCache(photo.urls?.regular.orEmpty(), binding.previewIV)
